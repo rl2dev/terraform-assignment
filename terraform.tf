bdfa730b5920f6b339ecfe5c6e360f9008d4ce3e
rl2dev <rinelle.lin@thoughtworks.com>
2026-03-12 15:53:22 +0800

    Update versions

--- terraform.tf
+++ terraform.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.34"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.2"
+  required_version = ">= 1.3"
 }
